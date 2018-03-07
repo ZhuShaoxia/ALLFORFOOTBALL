1797cef6e5187e601ec17382ed9ee9795fb02fec
zhuxiaolei <524868226@qq.com>
2018-03-07 15:39:39 +0800

    更新用户信息

---
+++ src/main/java/com/ccsu/utils/MD5Util.java
@@ -0,0 +1,42 @@
+package com.ccsu.utils;
+
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: Xiaolei Zhu
+ * @Date: 2018/3/1
+ * @Time: 11:13
+ * Description:
+ */
+public class MD5Util {
+    public static void main(String[] args) throws Exception {
+        String password = "123";
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        BASE64Encoder base64en = new BASE64Encoder();
+        password = base64en.encode(md5.digest(password.getBytes("utf-8")));
+        System.out.println(password);
+    }
+
+    public static String MD5Encode(String password) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        BASE64Encoder base64en = new BASE64Encoder();
+        try {
+            password = base64en.encode(md5.digest(password.getBytes("utf-8")));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return password;
+    }
+}
+
