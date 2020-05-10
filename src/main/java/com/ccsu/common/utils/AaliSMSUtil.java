3b24f9c3b14e6f88d501dfb2baea96f59c5d1002
少侠我姓朱 <mr.zhuxiaolei@gmail.com>
2020-05-10 10:27:28 +0800

    Update AaliSMSUtil.java

--- src/main/java/com/ccsu/common/utils/AaliSMSUtil.java
+++ src/main/java/com/ccsu/common/utils/AaliSMSUtil.java
@@ -29,8 +29,8 @@ public class AaliSMSUtil {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     //  此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAI3ouzXFvTJDvh";
-    static final String accessKeySecret = "pHczpggmgiObsMg4wJGNpGpzoUrz1y";
+    static final String accessKeyId = "**";
+    static final String accessKeySecret = "**";
 
     public static SendSmsResponse sendSms() throws ClientException {
 
@@ -46,7 +46,7 @@ public class AaliSMSUtil {
         //组装请求对象-具体描述见控制台-文档部分内容
         SendSmsRequest request = new SendSmsRequest();
         //必填:待发送手机号
-        request.setPhoneNumbers("15211054977");
+        request.setPhoneNumbers("**");
         //必填:短信签名-可在短信控制台中找到
         request.setSignName("朱晓磊");
         //必填:短信模板-可在短信控制台中找到
@@ -81,7 +81,7 @@ public class AaliSMSUtil {
         //必填:待发送手机号
         request.setPhoneNumbers(user.getPhone());
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("朱晓磊");
+        request.setSignName("**");
         //必填:短信模板-可在短信控制台中找到
         request.setTemplateCode("SMS_133830004");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
@@ -107,7 +107,7 @@ public class AaliSMSUtil {
         //组装请求对象
         QuerySendDetailsRequest request = new QuerySendDetailsRequest();
         //必填-号码
-        request.setPhoneNumber("15211054977");
+        request.setPhoneNumber("**");
         //可选-流水号
         request.setBizId(bizId);
         //必填-发送日期 支持30天内记录查询，格式yyyyMMdd
